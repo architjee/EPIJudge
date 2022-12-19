f9c6536fea4810e131c881c587f27aceb65e3f4f
Archit Jain <32292295+architjee@users.noreply.github.com>
2022-12-19 12:44:46 +0530

    tree from preorder with null gone wrong

--- epi_judge_python/tree_from_preorder_with_null.py
+++ epi_judge_python/tree_from_preorder_with_null.py
@@ -8,7 +8,23 @@ from test_framework.test_utils import enable_executor_hook
 
 def reconstruct_preorder(preorder: List[int]) -> BinaryTreeNode:
     # TODO - you fill in here.
-    return BinaryTreeNode()
+    ## ----- THE FOLLOWING CODE WILL NEVER WORK -------- BECAUSE THE APPROACH IS FLAWED
+    node_array = [None for x in preorder]
+    for index, node_value in enumerate(preorder):
+        if node_value:
+            node_array[index] = BinaryTreeNode(node_value)
+    def helper(i):
+        if not node_array[i]:
+            return 
+        if 2*i+1 < len(node_array):
+            node_array[i].left = node_array[2*i+1]
+            helper(2*i+1)
+        if 2*i+2 < len(node_array):
+            node_array[i].right = node_array[2*i+2]
+            helper(2*i+2)
+    helper(0)
+    # print(node_array[0].left)
+    return node_array[0]
 
 
 @enable_executor_hook
