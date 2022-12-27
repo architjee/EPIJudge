a6acf87c623660ed56f5f1dcee462e7baca9cb15
Archit Jain <32292295+architjee@users.noreply.github.com>
2022-12-27 20:30:47 +0530

    longest contained interval.py

--- epi_judge_python/longest_contained_interval.py
+++ epi_judge_python/longest_contained_interval.py
@@ -4,8 +4,22 @@ from test_framework import generic_test
 
 
 def longest_contained_range(A: List[int]) -> int:
-    # TODO - you fill in here.
-    return 0
+    unprocessed_entries = set(A)
+    max_interval_size = 0
+    while unprocessed_entries:
+        a = unprocessed_entries.pop()
+
+        ## Find the lower bound of the largest range containing a
+        lower = a-1
+        while lower in unprocessed_entries:
+            unprocessed_entries.remove(lower)
+            lower -= 1
+        upper = a+1
+        while upper in unprocessed_entries:
+            unprocessed_entries.remove(upper)
+            upper +=1
+        max_interval_size= max(max_interval_size, upper-lower-1)
+    return max_interval_size
 
 
 if __name__ == '__main__':
