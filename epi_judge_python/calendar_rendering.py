e17a9b73446e92f4c87ba619374737de8b6f985e
Archit Jain <32292295+architjee@users.noreply.github.com>
2022-12-28 19:01:08 +0530

    calendar rendering maximum overlapping interval count .py

--- epi_judge_python/calendar_rendering.py
+++ epi_judge_python/calendar_rendering.py
@@ -11,7 +11,24 @@ Event = collections.namedtuple('Event', ('start', 'finish'))
 
 def find_max_simultaneous_events(A: List[Event]) -> int:
     # TODO - you fill in here.
-    return 0
+    endpoints = []
+    Endpoint = collections.namedtuple('Endpoint', ('time', 'is_start'))
+    for event in A:
+        e1 = Endpoint(event.start, True)
+        e2 = Endpoint(event.finish, False)
+        endpoints.append(e1)
+        endpoints.append(e2)
+    endpoints.sort(key= lambda x: (x.time,not x.is_start))
+    count = 0
+    ans = 0
+    for e in endpoints:
+        if e.is_start:
+            count +=1
+            ans = max(ans, count)
+        else:
+            count -=1
+    return ans
+
 
 
 @enable_executor_hook
