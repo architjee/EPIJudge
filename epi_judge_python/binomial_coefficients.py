f9af589bc0f2d453f64a000f73c957babf2dcab5
Archit Jain <32292295+architjee@users.noreply.github.com>
2022-12-31 08:48:39 +0530

    binomial coefficients code working.py

--- epi_judge_python/binomial_coefficients.py
+++ epi_judge_python/binomial_coefficients.py
@@ -3,7 +3,16 @@ from test_framework import generic_test
 
 def compute_binomial_coefficient(n: int, k: int) -> int:
     # TODO - you fill in here.
-    return 0
+    d={}
+    def aux_binomial_coefficient(x, y):
+        ## The function is x choose y.
+        if y==0 or y==x:
+            return 1
+        key = (x, y)
+        if key not in d:
+            d[key] = aux_binomial_coefficient(x-1, y)+aux_binomial_coefficient(x-1, y-1)
+        return d[key]
+    return aux_binomial_coefficient(n, k)
 
 
 if __name__ == '__main__':
